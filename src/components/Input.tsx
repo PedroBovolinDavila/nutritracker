6f07b0c501880941cd2e578fa05edb026817fbf9
Pedro Davila <pedrodavila.bovolin@gmail.com>
2023-01-20 02:08:39 -0300

    styles: update input and textarea styles

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -5,7 +5,7 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
 export function Input({ ...rest }: InputProps) {
   return (
     <input
-      className="text-slate-100 rounded-md bg-slate-900 border-2 border-slate-700 p-2 placeholder:text-slate-400 text-lg outline-none hover:border-teal-500/50 focus:border-teal-500 transition-all"
+      className="text-slate-200 rounded-md bg-slate-900 border-2 border-slate-700 py-2 px-4 h-max placeholder:text-slate-500 text-lg outline-none hover:border-slate-700 focus:border-teal-500 transition-all"
       {...rest}
     />
   )
