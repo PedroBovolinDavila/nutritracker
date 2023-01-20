3705cc73f39f067647b49a780a9c6d8f3a18e333
Pedro Davila <pedrodavila.bovolin@gmail.com>
2023-01-20 18:31:48 -0300

    styles: change input border config

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -5,7 +5,7 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
 export function Input({ ...rest }: InputProps) {
   return (
     <input
-      className="text-slate-200 w-full rounded-md bg-slate-900 border-2 border-slate-700 py-2 px-4 h-max placeholder:text-slate-500 text-lg outline-none hover:border-slate-700 focus:border-teal-500 transition-all"
+      className="text-slate-200 w-full rounded-t-md bg-slate-900 border-b-2 border-transparent border-b-slate-700 py-2 px-4 h-max placeholder:text-slate-500 text-lg outline-none hover:border-b-teal-500/50 focus:border-teal-500 transition-all"
       {...rest}
     />
   )
