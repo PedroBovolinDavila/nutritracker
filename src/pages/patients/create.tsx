e747d63bf56130bc2030edc4117527a19df67567
Pedro Davila <pedrodavila.bovolin@gmail.com>
2023-01-20 02:12:09 -0300

    refactor: changes the create patient screen to new input

--- src/pages/patients/create.tsx
+++ src/pages/patients/create.tsx
@@ -1,6 +1,7 @@
 import { Button } from '../../components/Button'
 import { Input } from '../../components/Input'
 import { Navbar } from '../../components/Navbar'
+import { TextArea } from '../../components/TextArea'
 
 export default function CreatePatient() {
   return (
@@ -10,31 +11,14 @@ export default function CreatePatient() {
       <div className="w-full mt-4 max-w-screen-xl mx-auto bg-slate-800 border-2 border-slate-700 rounded-md p-4 flex flex-col gap-4">
         <h3 className="text-lg font-bold text-slate-200">Adicionar cliente</h3>
         <div className="grid grid-cols-2 gap-4">
-          <Input label="Nome completo" />
-          <Input label="E-mail" />
-          <Input label="Username" />
-          <Input label="Idade" />
-          <Input label="Senha de acesso" />
-          <Input label="Repita a senha" />
-          <div className="flex flex-col gap-2 h-max">
-            <label htmlFor="avatar" className="text-slate-200">
-              Foto
-            </label>
-            <input
-              type="file"
-              id="avatar"
-              className="text-slate-100 rounded-md bg-slate-700 p-2 border-2 border-slate-600 outline-none hover:border-teal-500/50 focus:border-teal-500 transition-all"
-            />
-          </div>
-          <div className="flex flex-col gap-2 h-max">
-            <label htmlFor="observations" className="text-slate-200">
-              Observações
-            </label>
-            <textarea
-              id="observations"
-              className="text-slate-100 rounded-md bg-slate-700 p-2 border-2 border-slate-600 outline-none hover:border-teal-500/50 focus:border-teal-500 transition-all"
-            />
-          </div>
+          <Input placeholder="Nome completo" />
+          <Input placeholder="E-mail" />
+          <Input placeholder="Username" />
+          <Input placeholder="Idade" />
+          <Input placeholder="Senha de acesso" />
+          <Input placeholder="Repita a senha" />
+          <Input type="file" placeholder="Avatar" />
+          <TextArea placeholder="Observações" />
         </div>
         <Button>Criar paciente</Button>
       </div>
