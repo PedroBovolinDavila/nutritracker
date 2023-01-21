67592e4d51234a5598022cfbbac69c9732f00f03
Pedro Davila <pedrodavila.bovolin@gmail.com>
2023-01-21 02:06:48 -0300

    feat: create doctor sign up screen

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,12 +1,42 @@
 import { useRouter } from 'next/router'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 import { Button } from '../../components/Button'
 import { Input } from '../../components/Input'
 import { MultiStep } from '../../components/MultiStep'
+import { api } from '../../lib/axios'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { localStorageConfig } from '../../config/localStorage'
+
+const signUpFormSchema = z.object({
+  name: z.string(),
+  lastName: z.string(),
+  age: z
+    .string()
+    .transform((age) => Number(age))
+    .refine((age) => age >= 18, { message: 'Você deve ser maior de idade.' }),
+  email: z.string().email({ message: 'Informe um e-mail válido.' }),
+})
+
+type SignUpFormData = z.infer<typeof signUpFormSchema>
 
 export default function SignUp() {
+  const { register, handleSubmit } = useForm<SignUpFormData>({
+    resolver: zodResolver(signUpFormSchema),
+  })
+
   const router = useRouter()
 
-  async function handleNextStep() {
+  async function handleSignUp(formData: SignUpFormData) {
+    const { data } = await api.post('/doctors', {
+      name: formData.name,
+      email: formData.email,
+      lastName: formData.lastName,
+      age: formData.age,
+    })
+
+    localStorage.setItem(localStorageConfig.doctorIdKey, data.content.doctorId)
+
     await router.push('/signup/register')
   }
 
@@ -30,13 +60,13 @@ export default function SignUp() {
 
           <div>
             <div className="grid grid-cols-2 my-6 gap-2">
-              <Input placeholder="Nome" />
-              <Input placeholder="Sobrenome" />
-              <Input placeholder="Idade" type="number" />
-              <Input placeholder="E-mail" type="email" />
+              <Input placeholder="Nome" {...register('name')} />
+              <Input placeholder="Sobrenome" {...register('lastName')} />
+              <Input placeholder="Idade" type="number" {...register('age')} />
+              <Input placeholder="E-mail" type="email" {...register('email')} />
             </div>
 
-            <Button onClick={handleNextStep}>Proximo passo</Button>
+            <Button onClick={handleSubmit(handleSignUp)}>Proximo passo</Button>
           </div>
         </div>
       </div>
