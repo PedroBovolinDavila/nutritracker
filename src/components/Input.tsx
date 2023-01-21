b5ecee1fdc0a41c74a251ff3791eeaf39e3d1d44
Pedro Davila <pedrodavila.bovolin@gmail.com>
2023-01-21 00:26:23 -0300

    feat: add forwardRef in input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,12 +1,15 @@
-import { InputHTMLAttributes } from 'react'
+import { forwardRef, InputHTMLAttributes } from 'react'
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
-export function Input({ ...rest }: InputProps) {
-  return (
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ ...rest }, ref) => (
     <input
       className="text-slate-200 w-full rounded-t-md bg-slate-900 border-b-2 border-transparent border-b-slate-700 py-2 px-4 h-max placeholder:text-slate-500 text-lg outline-none hover:border-b-teal-500/50 focus:border-teal-500 transition-all"
+      ref={ref}
       {...rest}
     />
-  )
-}
+  ),
+)
+
+Input.displayName = 'Input'
