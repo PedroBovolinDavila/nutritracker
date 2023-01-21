c4573cf205a7babe0857dead4e4089a842d6fdf0
Pedro Davila <pedrodavila.bovolin@gmail.com>
2023-01-21 02:07:04 -0300

    feat: create doctor register screen

--- src/pages/signup/register.tsx
+++ src/pages/signup/register.tsx
@@ -1,12 +1,47 @@
+import { zodResolver } from '@hookform/resolvers/zod'
 import { useRouter } from 'next/router'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 import { Button } from '../../components/Button'
 import { Input } from '../../components/Input'
 import { MultiStep } from '../../components/MultiStep'
+import { localStorageConfig } from '../../config/localStorage'
+import { api } from '../../lib/axios'
+
+const registerFormSchema = z.object({
+  username: z.string(),
+  password: z
+    .string()
+    .min(6, { message: 'Sua senha deve ter no mínimo 6 caracteres' }),
+})
+
+type RegisterFormData = z.infer<typeof registerFormSchema>
 
 export default function Register() {
+  const { register, handleSubmit } = useForm<RegisterFormData>({
+    resolver: zodResolver(registerFormSchema),
+  })
+
   const router = useRouter()
 
-  async function handleNextStep() {
+  async function handleRegisterDoctor(formData: RegisterFormData) {
+    const doctorId = localStorage.getItem(localStorageConfig.doctorIdKey)
+
+    if (!doctorId) {
+      await router.push('/signup')
+    }
+
+    await api.patch(
+      '/doctors/register',
+      {
+        username: formData.username,
+        password: formData.password,
+      },
+      {
+        params: { doctorId },
+      },
+    )
+
     await router.push('/signup/add-description')
   }
 
@@ -30,11 +65,17 @@ export default function Register() {
 
           <div>
             <div className="flex flex-col gap-2 my-6">
-              <Input placeholder="Username" />
-              <Input placeholder="Senha" type="password" />
+              <Input placeholder="Username" {...register('username')} />
+              <Input
+                placeholder="Senha"
+                type="password"
+                {...register('password')}
+              />
             </div>
 
-            <Button onClick={handleNextStep}>Proximo passo</Button>
+            <Button onClick={handleSubmit(handleRegisterDoctor)}>
+              Proximo passo
+            </Button>
           </div>
         </div>
       </div>
