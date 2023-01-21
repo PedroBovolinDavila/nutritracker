11f5d3f2088c13fe2fd25b4db107f48b6f6d22da
Pedro Davila <pedrodavila.bovolin@gmail.com>
2023-01-21 01:36:20 -0300

    feat: add forwardRef in textarea

--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -1,12 +1,15 @@
-import { TextareaHTMLAttributes } from 'react'
+import { forwardRef, TextareaHTMLAttributes } from 'react'
 
 interface TextAreaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {}
 
-export function TextArea({ ...rest }: TextAreaProps) {
-  return (
+export const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
+  ({ ...rest }, ref) => (
     <textarea
       className="text-slate-100 rounded-md bg-slate-900 border-2 border-slate-700 p-2 placeholder:text-slate-500 text-lg outline-none hover:border-slate-700 focus:border-teal-500 transition-all"
+      ref={ref}
       {...rest}
     />
-  )
-}
+  ),
+)
+
+TextArea.displayName = 'TextArea'
