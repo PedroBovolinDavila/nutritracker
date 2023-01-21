7e777024e1d0663a77060febd3b5c594b5c8ff02
Pedro Davila <pedrodavila.bovolin@gmail.com>
2023-01-21 18:41:31 -0300

    feat: add custom styles if have an error in the form

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,14 +1,24 @@
 import { forwardRef, InputHTMLAttributes } from 'react'
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  errorMessage?: string
+}
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ ...rest }, ref) => (
-    <input
-      className="text-slate-200 w-full rounded-t-md bg-slate-900 border-b-2 border-transparent border-b-slate-700 py-2 px-4 h-max placeholder:text-slate-500 text-lg outline-none hover:border-b-teal-500/50 focus:border-teal-500 transition-all"
-      ref={ref}
-      {...rest}
-    />
+  ({ errorMessage, ...rest }, ref) => (
+    <div>
+      <input
+        className={`text-slate-200 w-full rounded-t-md bg-slate-900 border-b-2 py-2 px-4 h-max placeholder:text-slate-500 text-lg outline-none hover:border-b-teal-500/50 focus:border-teal-500 transition-all ${
+          errorMessage ? 'border-red-600' : 'border-b-slate-700 '
+        }`}
+        ref={ref}
+        {...rest}
+      />
+
+      {errorMessage && (
+        <span className="text-red-600 mt-1">{errorMessage}</span>
+      )}
+    </div>
   ),
 )
 
--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -1,14 +1,24 @@
 import { forwardRef, TextareaHTMLAttributes } from 'react'
 
-interface TextAreaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {}
+interface TextAreaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+  errorMessage?: string
+}
 
 export const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
-  ({ ...rest }, ref) => (
-    <textarea
-      className="text-slate-100 rounded-md bg-slate-900 border-2 border-slate-700 p-2 placeholder:text-slate-500 text-lg outline-none hover:border-slate-700 focus:border-teal-500 transition-all"
-      ref={ref}
-      {...rest}
-    />
+  ({ errorMessage, ...rest }, ref) => (
+    <div>
+      <textarea
+        className={`text-slate-200 w-full rounded-t-md bg-slate-900 border-b-2 py-2 px-4 h-max placeholder:text-slate-500 text-lg outline-none hover:border-b-teal-500/50 focus:border-teal-500 transition-all ${
+          errorMessage ? 'border-red-600' : 'border-b-slate-700 '
+        }`}
+        ref={ref}
+        {...rest}
+      />
+
+      {errorMessage && (
+        <span className="text-red-600 mt-1">{errorMessage}</span>
+      )}
+    </div>
   ),
 )
 
