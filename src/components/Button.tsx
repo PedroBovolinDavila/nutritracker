d673c61cb4532376eb9f0c84a95966ec59d8ba3d
Pedro Davila <pedrodavila.bovolin@gmail.com>
2023-01-22 00:26:00 -0300

    feat: add button variants

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,28 @@
 import { ButtonHTMLAttributes, ReactNode } from 'react'
 
+type ButtonVariants = 'primary' | 'secondary'
+
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode
+  small?: boolean
+  variant?: ButtonVariants
 }
 
-export function Button({ children, ...rest }: ButtonProps) {
+export function Button({
+  children,
+  small = false,
+  variant = 'primary',
+  ...rest
+}: ButtonProps) {
   return (
     <button
-      className="bg-teal-500 w-full hover:bg-teal-600 font-bold transition-all rounded-md p-2 disabled:bg-teal-700 disabled:cursor-not-allowed"
+      className={` font-bold transition-all rounded-md p-2  disabled:cursor-not-allowed ${
+        small ? 'w-max px-4' : 'w-full'
+      } ${
+        variant === 'primary'
+          ? 'bg-teal-500 hover:bg-teal-600 disabled:bg-teal-700'
+          : 'bg-slate-400 hover:bg-slate-500 disabled:bg-slate-600'
+      }`}
       {...rest}
     >
       {children}
