9ea8e1c8f918941148f484e3d7f248365f7c65eb
Pedro Davila <pedrodavila.bovolin@gmail.com>
2023-01-21 22:00:34 -0300

    styles: add disabled styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,7 +7,7 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 export function Button({ children, ...rest }: ButtonProps) {
   return (
     <button
-      className="bg-teal-500 w-full hover:bg-teal-600 font-bold transition-all rounded-md p-2"
+      className="bg-teal-500 w-full hover:bg-teal-600 font-bold transition-all rounded-md p-2 disabled:bg-teal-700 disabled:cursor-not-allowed"
       {...rest}
     >
       {children}
