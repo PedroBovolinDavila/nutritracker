cfda2802c1e4651ba6aa8c826a97bbc95d718383
Pedro Davila <pedrodavila.bovolin@gmail.com>
2023-01-22 11:57:32 -0300

    feat: add large variant in modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -3,14 +3,19 @@ import { ReactNode } from 'react'
 interface ModalProps {
   children: ReactNode
   isOpen: boolean
+  large?: boolean
 }
 
-export function Modal({ children, isOpen }: ModalProps) {
+export function Modal({ children, isOpen, large = false }: ModalProps) {
   return (
     <>
       {isOpen && (
         <div className="absolute left-0 top-0 w-full h-screen bg-black/60 flex items-center justify-center">
-          <div className="bg-slate-800 border-2 border-slate-700 rounded-md p-4 w-full max-w-xl">
+          <div
+            className={`bg-slate-800 border-2 border-slate-700 rounded-md p-4 w-full relative ${
+              large ? 'max-w-3xl' : 'max-w-xl'
+            }`}
+          >
             {children}
           </div>
         </div>
