4c0755aff1290824488119b8059da96d71b531c7
Pedro Davila <pedrodavila.bovolin@gmail.com>
2023-01-23 20:06:26 -0300

    feat: add forwardRef in select

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,23 +1,37 @@
-import { SelectHTMLAttributes } from 'react'
+import { forwardRef, SelectHTMLAttributes } from 'react'
 
 interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
   options: {
     value: string
     displayValue: string
   }[]
+  errorMessage?: string
 }
 
-export default function Select({ options, ...rest }: SelectProps) {
-  return (
-    <select
-      className="text-slate-200 border-b-slate-700 w-full rounded-t-md bg-slate-900 border-b-2 py-2 px-4 h-max placeholder:text-slate-500 text-lg outline-none hover:border-b-teal-500/50 focus:border-teal-500 transition-all"
-      {...rest}
-    >
-      {options.map((option) => (
-        <option key={option.value} value={option.value}>
-          {option.displayValue}
-        </option>
-      ))}
-    </select>
-  )
-}
+export const Select = forwardRef<HTMLSelectElement, SelectProps>(
+  ({ options, errorMessage, ...rest }, ref) => {
+    return (
+      <div>
+        <select
+          className={`text-slate-200 w-full rounded-t-md bg-slate-900 border-b-2 py-2 px-4 h-max placeholder:text-slate-500 text-lg outline-none hover:border-b-teal-500/50 focus:border-teal-500 transition-all ${
+            errorMessage ? 'border-red-600' : 'border-b-slate-700 '
+          }`}
+          ref={ref}
+          {...rest}
+        >
+          {options.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.displayValue}
+            </option>
+          ))}
+        </select>
+
+        {errorMessage && (
+          <span className="text-red-600 mt-1">{errorMessage}</span>
+        )}
+      </div>
+    )
+  },
+)
+
+Select.displayName = 'Select'
