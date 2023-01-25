c2a9e79dafe897b5370afc2c4c67cce77074d2ac
Pedro Davila <pedrodavila.bovolin@gmail.com>
2023-01-25 16:31:04 -0300

    feat: add alert icon in navbar

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -3,12 +3,16 @@ import { ReactNode } from 'react'
 interface IconButtonProps {
   icon: ReactNode
   title: string
+  alert?: boolean
 }
 
-export function IconButton({ icon, title }: IconButtonProps) {
+export function IconButton({ icon, title, alert = false }: IconButtonProps) {
   return (
-    <>
+    <div className="flex items-center relative">
+      {alert && (
+        <span className="bg-teal-500 absolute rounded-full w-2 h-2 top-0 right-0" />
+      )}
       <button title={title}>{icon}</button>
-    </>
+    </div>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import { Gear, Power } from 'phosphor-react'
+import { Gear, Power, Bell } from 'phosphor-react'
 import { IconButton } from './IconButton'
 import { motion } from 'framer-motion'
 
@@ -10,12 +10,12 @@ export function Navbar() {
       whileInView={{ opacity: 1, y: 0, transition: { duration: 0.8 } }}
       className="w-full max-w-screen-xl mx-auto bg-slate-800 border-2 border-slate-700 rounded-md p-4 flex items-center justify-between"
     >
-      {/* Header */}
       <Link href="/" className="font-bold text-2xl text-slate-200">
         Nutri
         <span className="text-teal-400">Tracker</span>
       </Link>
       <div className="flex gap-2 text-xl text-slate-300">
+        <IconButton icon={<Bell />} title="Ver alertas" alert />
         <IconButton icon={<Gear />} title="Configurações da conta" />
         <IconButton icon={<Power />} title="Sair da plataforma" />
       </div>
