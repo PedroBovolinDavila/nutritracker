a9f7b7e8b2b375b1e4c91a5cfcf683161e1207c0
Pedro Davila <pedrodavila.bovolin@gmail.com>
2023-01-28 01:54:02 -0300

    feat: create getPatientData function

---
+++ src/utils/getPatientData.ts
@@ -0,0 +1,43 @@
+import { GetServerSidePropsContext } from 'next'
+import { parseCookies } from 'nookies'
+import { api } from '../lib/axios'
+import { prisma } from '../lib/prisma'
+
+export async function getPatientData(ctx: GetServerSidePropsContext) {
+  const { req } = ctx
+
+  const { '@nutritracker-auth': token } = parseCookies({ req })
+
+  if (!token) {
+    return {
+      redirectUrl: '/signin',
+    }
+  }
+
+  const { data } = await api.post('http://localhost:3000/api/validate-token', {
+    token,
+  })
+
+  if (data.role === 'doctor') {
+    return {
+      redirectUrl: '/doctor',
+    }
+  }
+
+  const patient = await prisma.patients.findUnique({
+    where: {
+      id: data.id,
+    },
+    include: {
+      doctor: true,
+    },
+  })
+
+  if (!patient) {
+    return {
+      redirectUrl: '/signup',
+    }
+  }
+
+  return { patient }
+}
