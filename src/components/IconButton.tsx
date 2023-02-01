88b9ee313c1f00c7ce092af39aaa79ff8ba0a2e3
Pedro <pedrodavila.bovolin@gmail.com>
2023-02-01 03:58:20 -0300

    feat: add onclick prop

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -4,15 +4,23 @@ interface IconButtonProps {
   icon: ReactNode
   title: string
   alert?: boolean
+  onClick?: () => void
 }
 
-export function IconButton({ icon, title, alert = false }: IconButtonProps) {
+export function IconButton({
+  icon,
+  title,
+  alert = false,
+  onClick,
+}: IconButtonProps) {
   return (
     <div className="flex items-center relative">
       {alert && (
         <span className="bg-teal-500 absolute rounded-full w-2 h-2 top-0 right-0" />
       )}
-      <button title={title}>{icon}</button>
+      <button onClick={onClick} title={title}>
+        {icon}
+      </button>
     </div>
   )
 }
