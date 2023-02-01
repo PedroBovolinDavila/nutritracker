b2542b53dcc3a3dcfd77059e3a429902584cad17
Pedro <pedrodavila.bovolin@gmail.com>
2023-02-01 03:58:57 -0300

    feat: add small variant

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -4,16 +4,22 @@ interface ModalProps {
   children: ReactNode
   isOpen: boolean
   large?: boolean
+  small?: boolean
 }
 
-export function Modal({ children, isOpen, large = false }: ModalProps) {
+export function Modal({
+  children,
+  isOpen,
+  large = false,
+  small = false,
+}: ModalProps) {
   return (
     <>
       {isOpen && (
         <div className="absolute left-0 top-0 w-full h-screen bg-black/60 flex items-center justify-center">
           <div
             className={`bg-slate-800 border-2 border-slate-700 rounded-md p-4 w-full relative ${
-              large ? 'max-w-3xl' : 'max-w-xl'
+              large ? 'max-w-3xl' : small ? 'max-w-md' : 'max-w-xl'
             }`}
           >
             {children}
