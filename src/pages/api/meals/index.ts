59aab4a2f60e6e383012bca567cc8b6d29daf636
Pedro <pedrodavila.bovolin@gmail.com>
2023-02-01 20:02:49 -0300

    feat: add image upload

--- src/pages/api/meals/index.ts
+++ src/pages/api/meals/index.ts
@@ -1,52 +1,77 @@
+import multer from 'multer'
 import { NextApiRequest, NextApiResponse } from 'next'
+import nextConnect from 'next-connect'
 import { z, ZodError } from 'zod'
 import { prisma } from '../../../lib/prisma'
+import { storage } from '../../../lib/storage'
 
 const createMealBodySchema = z.object({
   title: z.string().min(1, { message: 'Informe um titulo' }),
   description: z.string().min(1, { message: 'Informe uma descrição' }),
   patientId: z.string().uuid().min(1, { message: 'Informe um id' }),
-  ingredients: z.array(
-    z.object({
-      name: z.string(),
-      weight: z.number(),
-    }),
-  ),
+  ingredients: z.string().transform((ingredients) => JSON.parse(ingredients)),
 })
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse,
-) {
-  if (req.method !== 'POST') {
-    return res.status(405).end()
-  }
+const upload = multer({ storage })
 
+const apiRoute = nextConnect({
+  onNoMatch(req: NextApiRequest, res: NextApiResponse) {
+    return res.status(405).end()
+  },
+  onError(error, req: NextApiRequest, res: NextApiResponse) {
+    res
+      .status(501)
+      .json({ error: `Sorry something Happened! ${error.message}` })
+  },
+})
+
+apiRoute.use(upload.single('image'))
+
+interface Req extends NextApiRequest {
+  file: {
+    filename: string
+  }
+}
+
+apiRoute.post(async (req: Req, res: NextApiResponse) => {
   try {
     const { description, ingredients, patientId, title } =
       createMealBodySchema.parse(req.body)
+
+    const { filename } = req.file
+
+    if (!filename) {
+      return res.status(400).json({
+        message: 'Informe uma imagem para sua refeição.',
+      })
+    }
 
     const meal = await prisma.meal.create({
       data: {
         description,
         title,
         ingredients,
+        image_url: filename,
         patient_id: patientId,
       },
     })
 
-    return res.json({
-      meal,
+    return res.status(201).json({
+      mealId: meal.id,
     })
   } catch (err) {
     if (err instanceof ZodError) {
       return res.status(400).json({
-        message: err.issues[0].message,
-      })
-    } else {
-      return res.status(500).json({
-        message: 'Internal server error',
+        message: err.message,
       })
     }
   }
+})
+
+export default apiRoute
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
 }
