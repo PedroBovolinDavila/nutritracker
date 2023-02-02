7915f6a5bd70cb8cbeaa9e1fd8596e56b690a045
Pedro <pedrodavila.bovolin@gmail.com>
2023-02-02 02:16:08 -0300

    refactor: change foodcard to ingredientcard

--- src/components/meals/IngredientCard.tsx
+++ src/components/meals/IngredientCard.tsx
@@ -1,11 +1,18 @@
 import { Scales } from 'phosphor-react'
 
-export function FoodCard() {
+interface IngredientCardProps {
+  ingredient: {
+    name: string
+    weight: number
+  }
+}
+
+export function IngredientCard({ ingredient }: IngredientCardProps) {
   return (
     <div className="border-2 border-slate-700 hover:border-teal-400/70 transition-all w-max p-2 rounded-md text-slate-200">
-      <h3 className="text-md">Filé de frango</h3>
+      <h3 className="text-md">{ingredient.name}</h3>
       <div className="flex text-sm gap-1 items-center mt-1">
-        <Scales /> 120g
+        <Scales /> {ingredient.weight}g
       </div>
     </div>
   )
