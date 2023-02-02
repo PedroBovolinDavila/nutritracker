241957b09767f761a40b8da543c9d7b62871ae84
Pedro <pedrodavila.bovolin@gmail.com>
2023-02-02 02:16:37 -0300

    refactor: Change MealCard to Link

--- src/components/patients/MealCard.tsx
+++ src/components/patients/MealCard.tsx
@@ -1,21 +1,19 @@
 import Image from 'next/image'
-import { Clock } from 'phosphor-react'
-import { useRouter } from 'next/router'
+import Link from 'next/link'
+import { Meal } from '../../pages/patient'
 
-export function MealCard() {
-  const router = useRouter()
+interface MealCardProps {
+  meal: Meal
+}
 
-  async function handleOpenMealDetails() {
-    await router.push('/meals/refeicao1')
-  }
-
+export function MealCard({ meal }: MealCardProps) {
   return (
-    <div
+    <Link
+      href={`/meals/${meal.id}`}
       className="p-4 border-2 rounded-md border-slate-600 hover:border-teal-600 transition-all flex gap-4 cursor-pointer"
-      onClick={handleOpenMealDetails}
     >
       <Image
-        src="https://github.com/pedrobovolindavila.png"
+        src={`http://localhost:3000/uploads/${meal.image_url}`}
         width={120}
         height={120}
         className="rounded-md"
@@ -23,18 +21,11 @@ export function MealCard() {
       />
 
       <div>
-        <strong className="text-lg text-slate-300 font-bold">Refeição 1</strong>
-        <p className="text-slate-300">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias
-          delectus ea quia sint. Exercitationem assumenda, veritatis, mollitia
-          aliquam delectus in deleniti, laboriosam nostrum rerum architecto
-          veniam iste incidunt unde necessitatibus!
-        </p>
-
-        <div className="flex gap-1 items-center mt-4 text-slate-400">
-          <Clock /> 12:00
-        </div>
+        <strong className="text-lg text-slate-300 font-bold">
+          {meal.title}
+        </strong>
+        <p className="text-slate-300">{meal.description}</p>
       </div>
-    </div>
+    </Link>
   )
 }
