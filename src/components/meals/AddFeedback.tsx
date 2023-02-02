2b7f90f35253d736de6a8c076339dd405189a70a
Pedro <pedrodavila.bovolin@gmail.com>
2023-02-02 02:15:41 -0300

    fix: add fixed width

--- src/components/meals/AddFeedback.tsx
+++ src/components/meals/AddFeedback.tsx
@@ -4,7 +4,7 @@ import { TextArea } from '../TextArea'
 
 export function AddFeedback() {
   return (
-    <div className="bg-slate-800 border-2 border-slate-700 rounded-md p-4 flex flex-col gap-4">
+    <div className="bg-slate-800 border-2 border-slate-700 w-96 rounded-md p-4 flex flex-col gap-4">
       <h3 className="text-lg font-bold text-slate-200">Adicionar avaliação</h3>
       <div className="flex flex-col gap-4">
         <Input placeholder="Nota" />
