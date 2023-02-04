93cb3601467d66eea9af1c18530a040581892133
Pedro <pedrodavila.bovolin@gmail.com>
2023-02-04 18:46:20 -0300

    feat:  add AlertContextProvider in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import '../styles/main.css'
 import type { AppProps } from 'next/app'
+import { AlertsContextProvider } from '../contexts/AlertsContext'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AlertsContextProvider>
+      <Component {...pageProps} />
+    </AlertsContextProvider>
+  )
 }
